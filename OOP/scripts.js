bd22a8088fe88de51d7370b40f7c9e208427185e
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2026-05-08 09:55:35 +0900

    Correct typos in Person and Student constructors
    Fix typos and improve constructor function for Person and Student.

--- OOP/scripts.js
+++ OOP/scripts.js
@@ -681,30 +681,23 @@ rivian
 console.log(rivian.speedUS);
 */
 
-
-const Person = function (fistName, birthYear){
-    this.fistName = fistName;
-    this.birthYear = birthYear;
-
+const Person= function (firstName, birthYear){
+    this.firstName= firstName;
+    this.birthYear= birthYear;        
+}
+Person.prototype.calcAge= function(){
+    console.log(2037 - this.birthYear);    
 }
 
-const jonas = new Person('janos', 1991);
-console.log(jonas);
-
-const Person1= function (firstName, birthYear){
-    this,firstName= firstName;
-    this.birthYear= birthYear;
-
+const Student = function ( firstName, birthYear, course){
+    Person.call(this, firstName, birthYear);
+    this.course= course;
 }
-Person1.prototype.calcAge= function(){
-    console.log(2037  - this.birthYear);
-};
+Student.prototype= Object.create(Person.prototype);
 
-const Sturdent = function( firstName, birthYear, course){
-    Person1.call(this, firstName, birthYear);
-    this.course=course;
+Student.prototype.introduce= function(){
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
 }
- Sturdent.prototype.introdurce= function(){
-    console.log(`My name is ${this.firstName} and i study ${this.course}`);
- }
- const mike = new Sturdent( `Mike`, 2020, 'Computer Science')
+const mike =  new Student('Mike', 2020, 'Computer Sience');
+mike.introduce();
+mike.calcAge();
