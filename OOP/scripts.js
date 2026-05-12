039b7b8101c5b88142e2c56f64c167bbf77057fe
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2026-05-12 09:53:08 +0900

    Refactor to ES6 classes and improve functionality
    Refactor constructor functions and add ES6 class implementations for Person, Student, and Electric Car. Implement methods for acceleration, braking, and battery charging.

--- OOP/scripts.js
+++ OOP/scripts.js
@@ -757,3 +757,38 @@ john.introduce();
 const jane = new StudentCl('Jane Smith', 2000, 'Mathematics');
 jane.introduce();
 jane.calcAge();
+
+class Account {
+    constructor(owner, curency, pin){
+        this.owner= owner;
+        this.curency= curency;
+        this.pin= pin;
+        this.movements= [];
+        this.locale= navigation.language;
+        console.log(`thanks for opening an acouut, ${owner}`);
+
+    }
+    deposit(val){
+        this.movements.push(val);
+
+    }
+    withdraw(val){
+        this. deposit(-val);
+
+    }
+    approveLoan(val){
+        return true;
+        }
+    requersLoan(val) {          
+        if(this.approveLoan(val)){
+            this.deposit(val);
+            console.log(`loan approved`);
+        }
+    }
+}
+const acc1= new Account('Jonas', 'EUR', 1111);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.approveLoan(1000);
+acc1.requersLoan(1000);
+console.log(acc1);
